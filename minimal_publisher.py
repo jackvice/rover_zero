f3b72621ddb9cc64c1e871eba63b3a0a24e50d28
jackvice <jack.vice@gmail.com>
2023-07-23 11:45:01 -0400

    pushup

--- minimal_publisher.py
+++ minimal_publisher.py
@@ -53,7 +53,7 @@ class MinimalPublisher(Node):
 
         vel_cmd = Twist()
         vel_cmd.linear.x = -0.01 #self.const_linear_vel 
-        vel_cmd.angular.z = 0.0 #self.ang_vel
+        vel_cmd.angular.z = -0.01 #self.ang_vel
         self.publisher_v.publish(vel_cmd)
         #self.pub_cmd_vel.publish(vel_cmd)
 
