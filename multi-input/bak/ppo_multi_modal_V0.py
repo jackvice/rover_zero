e860c7a426d1a4290dba5cd143ba2405d4587934
jackvice <jack.vice@gmail.com>
2023-08-13 16:58:25 -0400

    pushup

--- multi-input/bak/ppo_multi_modal_V0.py
+++ multi-input/bak/ppo_multi_modal_V0.py
@@ -50,36 +50,6 @@ class PPOMemory:
         self.dones = []
         self.vals = []
 
-
-class Agent_atari(nn.Module):
-    def __init__(self, envs):
-        super().__init__()
-        self.network = nn.Sequential(
-            layer_init(nn.Conv2d(4, 32, 8, stride=4)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(32, 64, 4, stride=2)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
-            nn.ReLU(),
-            nn.Flatten(),
-            layer_init(nn.Linear(64 * 7 * 7, 512)),
-            nn.ReLU(),
-        )
-        self.actor = layer_init(nn.Linear(512, envs.single_action_space.n), std=0.01)
-        self.critic = layer_init(nn.Linear(512, 1), std=1)
-
-    def get_value(self, x):
-        return self.critic(self.network(x / 255.0))
-
-    def get_action_and_value(self, x, action=None):
-        hidden = self.network(x / 255.0)
-        logits = self.actor(hidden)
-        probs = Categorical(logits=logits)
-        if action is None:
-            action = probs.sample()
-        return action, probs.log_prob(action), probs.entropy(), self.critic(hidden)
-
-
 class ActorNetwork(nn.Module):
     def __init__(self, n_actions, input_dims, alpha,
             fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/ppo'):
