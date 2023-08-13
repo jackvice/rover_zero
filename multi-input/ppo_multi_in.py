d7885344f600a3017f27bac276478e81691bc476
jackvice <jack.vice@gmail.com>
2023-08-13 08:42:26 -0400

    pushup

--- multi-input/ppo_multi_in.py
+++ multi-input/ppo_multi_in.py
@@ -114,63 +114,6 @@ def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
-class Agent_atari(nn.Module):
-    def __init__(self, envs):
-        super().__init__()
-        self.network = nn.Sequential(
-            layer_init(nn.Conv2d(4, 32, 8, stride=4)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(32, 64, 4, stride=2)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
-            nn.ReLU(),
-            nn.Flatten(),
-            layer_init(nn.Linear(64 * 7 * 7, 512)),
-            nn.ReLU(),
-        )
-        self.actor = layer_init(nn.Linear(512, envs.single_action_space.n), std=0.01)
-        self.critic = layer_init(nn.Linear(512, 1), std=1)
-
-    def get_value(self, x):
-        return self.critic(self.network(x / 255.0))
-
-    def get_action_and_value(self, x, action=None):
-        hidden = self.network(x / 255.0)
-        logits = self.actor(hidden)
-        probs = Categorical(logits=logits)
-        if action is None:
-            action = probs.sample()
-        return action, probs.log_prob(action), probs.entropy(), self.critic(hidden)
-    
-class Agent_new(nn.Module):
-    def __init__(self, envs):
-        super().__init__()
-        self.network = nn.Sequential(
-            layer_init(nn.Conv2d(4, 32, 8, stride=4)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(32, 64, 4, stride=2)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
-            nn.ReLU(),
-            nn.Flatten(),
-            layer_init(nn.Linear(64 * 7 * 7, 512)),
-            nn.ReLU(),
-        )
-        self.actor = layer_init(nn.Linear(512, envs.single_action_space.n), std=0.01)
-        self.critic = layer_init(nn.Linear(512, 1), std=1)
-
-    def get_value(self, x):
-        return self.critic(self.network(x / 255.0))
-
-    def get_action_and_value(self, x, action=None):
-        hidden = self.network(x / 255.0)
-        logits = self.actor(hidden)
-        probs = Categorical(logits=logits)
-        if action is None:
-            action = probs.sample()
-        return action, probs.log_prob(action), probs.entropy(), self.critic(hidden)
-
-
 class Agent(nn.Module):
     def __init__(self, envs):
         super().__init__()
@@ -237,18 +180,9 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
 
-    # env setup
-    #envs = gym.vector.SyncVectorEnv(
-    #    [make_env(args.env_id, i, args.capture_video, run_name, args.gamma) for i in range(args.num_envs)]
-    #)
-    #assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
-
     envs = make_env(args.env_id, 0, args.capture_video, run_name, args.gamma)
-    #print(envs.__dir__())
-    #exit()
     assert isinstance(envs.action_space, gym.spaces.Box), "only continuous action space is supported"
     ##
-    #print("envs.observation_space.shape", envs.observation_space.shape)
     #print("envs.observation_space.shape", envs.observation_space.shape)
     #exit()
     ##
@@ -257,12 +191,7 @@ if __name__ == "__main__":
     optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
 
     # ALGO Logic: Storage setup
-    #obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
     #obs = torch.zeros((args.num_steps, args.num_envs) + envs.observation_space.shape).to(device)
-    #print('args.num_steps', args.num_steps, 'args.num_envs', args.num_envs, 'envs.observation_space.shape',
-    #      envs.observation_space.shape )
-    #print("obs.shape", obs.shape)
-    #exit()
     obs = torch.zeros(args.num_steps, envs.observation_space.shape[0]).to(device)
     #print("obs.shape", obs.shape)
     #exit()
