660be475277ded4fe6175ddc55b9f62bdeddb1c0
jackvice <jack.vice@gmail.com>
2024-02-15 21:04:12 -0500

    pushup

--- ppo_turtle.py
+++ ppo_turtle.py
@@ -81,8 +81,7 @@ def rover_main():
     print("environment made")
     assert isinstance(envs.action_space, gym.spaces.Box), \
         "only continuous action space is supported"
-    #assert isinstance(envs.single_action_space, gym.spaces.Box), \  # vec env
-        #"only continuous action space is supported"
+
     agent = Agent(envs).to(device)
     optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
 
@@ -141,14 +140,10 @@ def rover_main():
 
                 mb_advantages = b_advantages[mb_inds]
                 if args.norm_adv:
-                    mb_advantages = (mb_advantages - mb_advantages.mean()) \
-                        (mb_advantages.std() + 1e-8)
+                    mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)  # Fixed line
 
                 # Policy loss
                 pg_loss = calc_some_losses(mb_advantages, ratio, args)
-                #pg_loss1 = -mb_advantages * ratio
-                #pg_loss2 = -mb_advantages * torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
-                #pg_loss = torch.max(pg_loss1, pg_loss2).mean()
 
                 # Value loss
                 newvalue = newvalue.view(-1)
@@ -168,6 +163,50 @@ def rover_main():
             if args.target_kl is not None and approx_kl > args.target_kl:
                 break
 
+        """
+        for epoch in range(args.update_epochs):
+            np.random.shuffle(b_inds)
+            for start in range(0, args.batch_size, args.minibatch_size):
+                end = start + args.minibatch_size
+                mb_inds = b_inds[start:end]
+
+                _, newlogprob, entropy, newvalue = agent.get_action_and_value(b_obs[mb_inds],
+                                                                              b_actions[mb_inds])
+                logratio = newlogprob - b_logprobs[mb_inds]
+                ratio = logratio.exp()
+
+                with torch.no_grad():
+                    # calculate approx_kl http://joschu.net/blog/kl-approx.html
+                    old_approx_kl = (-logratio).mean()
+                    approx_kl = ((ratio - 1) - logratio).mean()
+                    clipfracs += [((ratio - 1.0).abs() > args.clip_coef).float().mean().item()]
+
+                mb_advantages = b_advantages[mb_inds]
+                if args.norm_adv:
+                    mb_advantages = (mb_advantages - mb_advantages.mean()) \
+                        (mb_advantages.std() + 1e-8)
+
+                # Policy loss
+                pg_loss = calc_some_losses(mb_advantages, ratio, args)
+
+                # Value loss
+                newvalue = newvalue.view(-1)
+                if args.clip_vloss:
+                    v_loss = clip_vloss(args, newvalue, b_returns, mb_inds, b_values)
+                else:
+                    v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
+
+                entropy_loss = entropy.mean()
+                loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
+
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(agent.parameters(), args.max_grad_norm)
+                optimizer.step()
+
+            if args.target_kl is not None and approx_kl > args.target_kl:
+                break
+        """
         y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
         var_y = np.var(y_true)
         explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
@@ -184,45 +223,20 @@ def rover_main():
 
 def make_env(env_id, capture_video, run_name, gamma):
     env = gym.make('TurtleBot3_Circuit_Simple_Continuous-v0') 
-    if capture_video:
-        env = gym.make(env_id)
-        #env = gym.make(env_id, render_mode="rgb_array")
-        #env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-    else:
-        env = gym.make(env_id)
-        env = gym.wrappers.FlattenObservation(env)  # deal with dm_control's Dict observation space
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env = gym.wrappers.ClipAction(env)
-        env = gym.wrappers.NormalizeObservation(env)
-        env = gym.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
-        env = gym.wrappers.NormalizeReward(env, gamma=gamma)
-        env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
-        #env = PixelObservationWrapper(env, pixels_only=False)
+
+    env = gym.make(env_id)
+    #env = gym.make(env_id, render_mode="rgb_array")
+    #env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+    
+    #Are these necessary?
+    env = gym.wrappers.ClipAction(env)
+    env = gym.wrappers.NormalizeObservation(env)
+    env = gym.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
+    env = gym.wrappers.NormalizeReward(env, gamma=gamma)
+    env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
     return env
 
     
-    
-def make_env_thunk(env_id, idx, capture_video, run_name, gamma):
-    def thunk():
-        env = gym.make('TurtleBot3_Circuit_Simple_Continuous-v0') 
-        if capture_video and idx == 0:
-            env = gym.make(env_id)
-            #env = gym.make(env_id, render_mode="rgb_array")
-            #env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-        else:
-            env = gym.make(env_id)
-        env = gym.wrappers.FlattenObservation(env)  # deal with dm_control's Dict observation space
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env = gym.wrappers.ClipAction(env)
-        env = gym.wrappers.NormalizeObservation(env)
-        env = gym.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
-        env = gym.wrappers.NormalizeReward(env, gamma=gamma)
-        env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
-        #env = PixelObservationWrapper(env, pixels_only=False)
-        return env
-
-    return thunk
-
 def initialize_env(args, envs, device):
     """
     Initializes the game by setting the global step, start time, and getting the initial observations 
@@ -248,8 +262,8 @@ def initialize_env(args, envs, device):
 
 def take_step(step, envs, action, device, rewards, global_step, writer):
     # TRY NOT TO MODIFY: execute the game and log data.
-    next_obs, reward, next_done, infos = envs.step(action.cpu().numpy())
-    #next_done = np.logical_or(terminations, truncations)
+    next_obs, reward, next_done, truncations, infos = envs.step(action.cpu().numpy())
+    next_done = np.logical_or(next_done, truncations)
     rewards[step] = torch.tensor(reward).to(device).view(-1)
     #next_obs, next_done = torch.Tensor(next_obs['state']).to(device), torch.Tensor(next_done).to(device)
     next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor([int(next_done)]).to(device)
